4d58027f4713a50d257e1e1115ff359057392c2f
aaron <aareid1999@gmail.com>
2026-03-21 04:12:10 -0600

    JavaDoc for stackADT methods size and toArray as well as peek method in the QueueDT

--- Assignment2-CreatingADTs-implementingDS-XMLparser/src/QueueADT.java
+++ Assignment2-CreatingADTs-implementingDS-XMLparser/src/QueueADT.java
@@ -1,7 +1,25 @@
+/**
+ * QueueADT.java
+ * 
+ * Defines the public contract for a queue abstract data type.
+ * A queue stores elements in FIFO order (First-In, First-Out).
+ * 
+ * @param <E> the type of elements stored in the queue
+ */
 
 public interface QueueADT<E>{
 	void enqueue(E element);
 	E dequeue();
+	
+	/**
+     * Returns the number of elements currently stored in the queue.
+     *
+     * <p><b>Pre-condition:</b> None.</p>
+     *
+     * <p><b>Post-condition:</b> The queue remains unchanged.</p>
+     *
+     * @return the number of elements in the queue
+     */
 	E peek();
 	boolean isEmpty();
 	int size();
