660ec55b5ab8e4dd4214c6f0a53250c9d38e38f5
Kaley <kaleyywood@gmail.com>
2026-03-22 14:24:15 -0600

    Add Javadoc for push, pop, peek, isEmpty

--- Assignment2-CreatingADTs-implementingDS-XMLparser/src/StackADT.java
+++ Assignment2-CreatingADTs-implementingDS-XMLparser/src/StackADT.java
@@ -7,10 +7,49 @@
 
 public interface StackADT<E> {
 
-	void push(E element);
-	E pop();
-	E peek();
-	boolean isEmpty();
+    /**
+     * Adds an element to the top of the stack.
+     *
+     * <p><b>Pre-condition:</b> element is not null.</p>
+     * <p><b>Post-condition:</b> element is at the top of the stack, size increases by 1.</p>
+     *
+     * @param element the value to add to the top of the stack
+     * @throws NullPointerException if element is null
+     */
+    void push(E element);
+
+    /**
+     * Removes and returns the element at the top of the stack.
+     *
+     * <p><b>Pre-condition:</b> The stack is not empty.</p>
+     * <p><b>Post-condition:</b> The top element is removed, size decreases by 1.</p>
+     *
+     * @return the element that was at the top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    E pop();
+
+    /**
+     * Returns the top element without removing it from the stack.
+     *
+     * <p><b>Pre-condition:</b> The stack is not empty.</p>
+     * <p><b>Post-condition:</b> The stack is unchanged.</p>
+     *
+     * @return the element currently at the top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    E peek();
+
+
+    /**
+     * Returns true if the stack contains no elements.
+     *
+     * <p><b>Pre-condition:</b> None.</p>
+     * <p><b>Post-condition:</b> The stack is unchanged.</p>
+     *
+     * @return true if the stack is empty, false otherwise
+     */
+    boolean isEmpty();
 	
 	/**
      * Returns the number of elements currently stored in the stack.
